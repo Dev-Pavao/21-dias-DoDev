e2fcd2f77f4aa678223ad85b14894967577cd15e
Matheus Pavao <matheuspavao2005@gmail.com>
2024-03-06 16:10:59 -0300

    Codigo mais limpo e melhorado

--- Dia 6/tabuada.js
+++ Dia 6/tabuada.js
@@ -3,7 +3,7 @@
 let num1 = Math.abs(parseInt(prompt("Deseja a tabuada de qual número? ")))
 
 for(let tabuada = num1; tabuada <= num1 + 2; tabuada++){
-  console.log("Tabuada do número: " + tabuada)
+  console.log("Tabuada do número " + tabuada)
 for (let multiplicador = 0; multiplicador <=10; multiplicador ++){
   console.log(tabuada + " X " + multiplicador + " = " + (tabuada * multiplicador))
 }
